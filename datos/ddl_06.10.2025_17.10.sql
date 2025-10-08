51b203f66d6272163aa9b06789415e3cbbc5cacb
AmandaAZ04 <amandakv2@hotmail.com>
2025-10-08 16:39:44 -0300

    sql

--- datos/ddl_06.10.2025_17.10.sql
+++ datos/ddl_06.10.2025_17.10.sql
@@ -1,7 +1,7 @@
 USE proyecto_banco;
 
-CREATE TABLE direccion IF NOT EXISTS(
-    id_direccion INTEGER AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS direccion (
+    id_direccion INT AUTO_INCREMENT,
     comuna VARCHAR(30) NOT NULL,
     calle VARCHAR(20) NOT NULL,
     departamento VARCHAR(20) NULL,
@@ -10,22 +10,22 @@ CREATE TABLE direccion IF NOT EXISTS(
     CONSTRAINT pk_direccion PRIMARY KEY (id_direccion)
 );
 
-CREATE TABLE cliente IF NOT EXISTS(
-    id_cliente INTEGER AUTO_INCREMENT,
-    id_direccion INTEGER AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS cliente (
+    id_cliente INT AUTO_INCREMENT,
+    id_direccion INT,
     nombre VARCHAR(30) NOT NULL,
     apellido VARCHAR(30) NOT NULL,
     rut VARCHAR(12) NOT NULL,
-    telefono INTEGER NOT NULL,
+    telefono INT NOT NULL,
     mail VARCHAR(20) NULL,
 
     CONSTRAINT pk_cliente PRIMARY KEY (id_cliente),
     CONSTRAINT fk_direccion FOREIGN KEY (id_direccion) REFERENCES direccion(id_direccion)
 );
 
-CREATE TABLE cuenta IF NOT EXISTS(
-    id_cliente INTEGER AUTO_INCREMENT,
-    id_cuenta INTEGER AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS cuenta (
+    id_cliente INT,
+    id_cuenta INT AUTO_INCREMENT,
     numero_c VARCHAR(15) NOT NULL,
     saldo FLOAT NOT NULL,
     fecha_apertura DATETIME NOT NULL,
@@ -36,18 +36,18 @@ CREATE TABLE cuenta IF NOT EXISTS(
     CONSTRAINT fk_cliente FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente)
 );
 
-CREATE TABLE cuenta_destino IF NOT EXISTS(
-    id_cuenta_destino AUTO_INCREMENT INTEGER NOT NULL,
+CREATE TABLE IF NOT EXISTS cuenta_destino (
+    id_cuenta_destino INT AUTO_INCREMENT,
     tipo_cuenta_destino VARCHAR(15) NOT NULL,
     estado_cuenta_destino BOOLEAN NOT NULL,
 
     CONSTRAINT pk_cuenta_destino PRIMARY KEY (id_cuenta_destino)
 );
 
-CREATE TABLE transaccion IF NOT EXISTS(
-    id_cuenta INTEGER AUTO_INCREMENT,
-    id_transaccion INTEGER AUTO_INCREMENT,
-    id_cuenta_destino INTEGER AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS transaccion (
+    id_cuenta INT,
+    id_transaccion INT AUTO_INCREMENT,
+    id_cuenta_destino INT,
     tipo_transaccion VARCHAR(12) NOT NULL,
     fecha_transaccion DATETIME NOT NULL,
     monto FLOAT NOT NULL,
