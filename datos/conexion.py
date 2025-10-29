44e8586b62657723a411378c52c9d8487331b83b
david <davidmanquilef777@gmail.com>
2025-10-29 13:33:58 -0300

    ahora funciona inicio 👻

--- datos/conexion.py
+++ datos/conexion.py
@@ -5,6 +5,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 #User, password, host, port, database con sus credenciales DB
-DATABASE_URL = "mysql+sqlconnector://root:@localhost:3306/proyecto_banco"
+#DATABASE_URL = "mysql+sqlconnector://root:@localhost:3306/proyecto_banco"
+DATABASE_URL = "mysql+mysqlconnector://root:@localhost:3306/proyecto_banco"
 motor_db = create_engine(DATABASE_URL, pool_pre_ping=True)
 Session = sessionmaker(bind=motor_db)
